6084d229eaddc722f861a58e2e93de87fb5b0c01
miftahunajat <miftahunajat@gmail.com>
2018-09-20 21:22:17 +0700

    mergin all branch

---
+++ app/src/main/java/com/squishydev/setoz/englishkidstalk/ui/splashscreen/SplashScreenActivity.java
@@ -0,0 +1,23 @@
+package com.squishydev.setoz.englishkidstalk.ui.splashscreen;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.squishydev.setoz.englishkidstalk.R;
+import com.squishydev.setoz.englishkidstalk.databinding.ActivityMainBinding;
+import com.squishydev.setoz.englishkidstalk.databinding.ActivitySplashScreenBinding;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySplashScreenBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_splash_screen);
+        binding.ivBackgroundSplash.setOnClickListener(view -> {
+            startActivity(new Intent(SplashScreenActivity.this,));
+        });
+    }
+}
