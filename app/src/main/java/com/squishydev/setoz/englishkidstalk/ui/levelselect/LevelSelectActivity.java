12e825e411cc3a69d5ae0aefe40c66dc4d004074
miftahunajat <miftahunajat@gmail.com>
2018-09-20 15:06:28 +0700

    Menambahkan halaman pilih level

---
+++ app/src/main/java/com/squishydev/setoz/englishkidstalk/ui/levelselect/LevelSelectActivity.java
@@ -0,0 +1,74 @@
+package com.squishydev.setoz.englishkidstalk.ui.levelselect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+
+import com.squishydev.setoz.englishkidstalk.R;
+
+public class LevelSelectActivity extends AppCompatActivity {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_select);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a LevelSelectFragment (defined as a static inner class below).
+            return LevelSelectFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+    }
+}
---
+++ app/src/main/java/com/squishydev/setoz/englishkidstalk/ui/levelselect/LevelSelectFragment.java
@@ -0,0 +1,53 @@
+package com.squishydev.setoz.englishkidstalk.ui.levelselect;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.squishydev.setoz.englishkidstalk.R;
+
+/**
+ * Created by miftahun on 9/18/18.
+ * Email : miftahunajat@gmail.com
+ * Github : https://github.com/miftahunajat/
+ */
+
+public class LevelSelectFragment extends Fragment {
+
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private int[] mLayouts = {R.layout.fragment_level_select_easy,
+            R.layout.fragment_level_select_medium,
+            R.layout.fragment_level_select_hard};
+
+    public LevelSelectFragment() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static LevelSelectFragment newInstance(int sectionNumber) {
+        LevelSelectFragment fragment = new LevelSelectFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        int index = getArguments().getInt(ARG_SECTION_NUMBER);
+        Log.v("Debug",index + "");
+        View rootView = inflater.inflate(mLayouts[index - 1], container, false);
+        return rootView;
+    }
+}
