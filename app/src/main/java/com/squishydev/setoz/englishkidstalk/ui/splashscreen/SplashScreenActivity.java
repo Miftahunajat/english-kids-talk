0f1e4ccd0feb4134fe17cb5773cf03b1e45fc4bc
citrazizah <azizahcitra012@gmail.com>
2018-09-27 19:18:03 +0700

    editing splash screen

--- app/src/main/java/com/squishydev/setoz/englishkidstalk/ui/splashscreen/SplashScreenActivity.java
+++ app/src/main/java/com/squishydev/setoz/englishkidstalk/ui/splashscreen/SplashScreenActivity.java
@@ -5,6 +5,8 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squishydev.setoz.englishkidstalk.R;
 import com.squishydev.setoz.englishkidstalk.databinding.ActivityMainBinding;
@@ -12,13 +14,18 @@ import com.squishydev.setoz.englishkidstalk.databinding.ActivitySplashScreenBind
 import com.squishydev.setoz.englishkidstalk.ui.inputnama.InputNamaActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
-
+//private TextView tv;
+//private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivitySplashScreenBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_splash_screen);
-        binding.imageView16.setOnClickListener(view -> {
-            startActivity(new Intent(SplashScreenActivity.this, InputNamaActivity.class));
-        });
+       // binding.imageView16.setOnClickListener(view -> {
+          //  startActivity(new Intent(SplashScreenActivity.this, InputNamaActivity.class));
+//        });
+
+
+
     }
+
 }
