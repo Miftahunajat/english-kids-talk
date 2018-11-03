a62f814f95635a937bba9735b774fa926e4de21b
septy <septy.aminatul@gmail.com>
2018-11-03 03:16:43 -0700

    store

--- app/src/main/java/com/squishydev/setoz/englishkidstalk/ui/menuselect/itemstoremenu/ItemStoreFragment.java
+++ app/src/main/java/com/squishydev/setoz/englishkidstalk/ui/menuselect/itemstoremenu/ItemStoreFragment.java
@@ -1,24 +1,39 @@
 package com.squishydev.setoz.englishkidstalk.ui.menuselect.itemstoremenu;
 
+import android.app.LauncherActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.squishydev.setoz.englishkidstalk.R;
+import com.squishydev.setoz.englishkidstalk.data.network.model.Item;
+import com.squishydev.setoz.englishkidstalk.databinding.FragmentItemStoreBinding;
 import com.squishydev.setoz.englishkidstalk.di.componen.ActivityComponent;
 import com.squishydev.setoz.englishkidstalk.ui.base.BaseFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
 public class ItemStoreFragment extends BaseFragment implements
-        ItemStoreMvpView {
+        ItemStoreMvpView, ItemStoreAdapter.OnItemClick
+{
 
     private static final String TAG = "ItemStoreFragment";
 
+    FragmentItemStoreBinding binding;
+
     @Inject
     ItemStoreMvpPresenter<ItemStoreMvpView> mPresenter;
+    ItemStoreAdapter itemStoreAdapter;
+    List<Item> itemList;
+
 
     public static ItemStoreFragment newInstance() {
         Bundle args = new Bundle();
@@ -31,14 +46,30 @@ public class ItemStoreFragment extends BaseFragment implements
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_item_store, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_item_store,container, false);
+        itemList = new ArrayList<>();
+        Item item = new Item();
+        item.setName("koonam");
+        item.setSnippet("gdyu");
+        item.setStar(100);
+        item.setImage("asd");
+        itemList.add(item);
+        itemList.add(item);
+        itemList.add(item);
+        itemList.add(item);
+        itemList.add(item);
+
+        itemStoreAdapter = new ItemStoreAdapter(itemList, this);
+
+        binding.rvItem.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
+        binding.rvItem.setAdapter(itemStoreAdapter);
 
         ActivityComponent component = getActivityComponent();
         if (component != null) {
             component.inject(this);
             mPresenter.onAttach(this);
         }
-        return view;
+        return binding.getRoot();
     }
 
     @Override
@@ -51,6 +82,11 @@ public class ItemStoreFragment extends BaseFragment implements
     public void onDestroyView() {
         mPresenter.onDetach();
         super.onDestroyView();
+    }
+
+    @Override
+    public void onClick(int position) {
+
     }
 }
 
